df9403eaf98b6dea2c4fa38021afa7f54375957a
Graham Fujisawa <sfrrki@gmail.com>
2023-01-23 14:10:41 -0800

    added optional url to visit under a specific, hardcoded webpage

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,6 +21,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase.b2xVn2/* What goes here? */ };
+  res.render("urls_show", templateVars);
 });
 
 
