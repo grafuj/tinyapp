552381ca0eba3fc5c1439fe09aa98a87d411e147
Graham Fujisawa <sfrrki@gmail.com>
2023-01-23 15:11:04 -0800

    added rand string function

--- express_server.js
+++ express_server.js
@@ -8,26 +8,48 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let fullhex = 'abcdefghijklmnoqrstuvwxyz0123456789';
+
+  //we want a hex string of 6 chars
+  let randStr = "";
+  for (let i = 0; i < 6; i++) {
+    randStr += fullhex[Math.floor(36 * Math.random())];
+  }
+  return randStr;
+}
+
+app.use(express.urlencoded({ extended: true })); //converts from raw buffer into string
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let id = generateRandomString();
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get(`/urls/${generateRandomString}`, (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase.b2xVn2/* What goes here? */ };
   res.render("urls_show", templateVars);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
