b604cf2e60609e158903208f45cbb2cbc39377ed
Graham Fujisawa <sfrrki@gmail.com>
2023-01-24 13:30:51 -0800

    added delete functionality on homepage

--- express_server.js
+++ express_server.js
@@ -47,6 +47,12 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
