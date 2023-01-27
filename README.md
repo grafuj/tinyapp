95c525a5775b1b6d2db18172bf56b9d78787a394
Graham Fujisawa <sfrrki@gmail.com>
2023-01-26 21:00:27 -0800

    adding direct link to long URLs on url/:id and appropriate html messages for incorrect short URLs. Adding updated npm commands in README

--- README.md
+++ README.md
@@ -17,5 +17,6 @@ TinyApp is a full stack web application built with Node and Express that allows 
 
 ## Getting Started
 
-- Install all dependencies (using the `npm install` command).
-- Run the development web server using the `node express_server.js` command.
+- Install all dependencies using the `npm install` command.
+- Run the development web server using the `npm start` command.
+- Run the testing in mocha and chai using the `npm run test` command.
--- express_server.js
+++ express_server.js
@@ -37,12 +37,9 @@ const userDatabase = {
 
 app.use(express.urlencoded({ extended: true })); //converts from raw buffer into string
 
-//leaving these two in for now, should re
+//send to urls, which will send to login if not logged in
 app.get("/", (req, res) => {
   res.redirect("/urls");
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
@@ -65,7 +62,7 @@ app.post("/urls", (req, res) => {
   const user = userDatabase[id];
 
   if (!user) {
-    return res.status(403).send("Only logged in users can shorten URLs");
+    return res.status(401).send("Only logged in users can shorten URLs");
   }
 
   let newId = generateRandomString();
@@ -124,7 +121,7 @@ app.post("/urls/:id/delete", (req, res) => {
   const user = userDatabase[id];
 
   if (!user) {
-    return res.status(403).send("Only logged in users can delete URLs");
+    return res.status(401).send("Only logged in users can delete URLs");
   }
 
   //get url ID
@@ -142,12 +139,81 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+
+app.get("/login", (req, res) => {
+  const id = req.session.user_id;
+  
+  if (id) {
+    console.log(id, 'circular')
+    res.redirect("/urls");
+    return;
+  }
+  const templateVars = {
+    user: null,
+  };
+  res.render("login", templateVars);
+});
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  const user = getUserByEmail(email, userDatabase);
+  if (!user) { //returned undefined or otherwise
+    return res.status(401).send('user not found');
+  }
+  
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.status(403).send('the passwords do not match');
+  }
+  req.session.user_id = user.id;
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/urls");
+});
+
+app.get('/u/:id', (req, res) => {
+  const userId = req.session.user_id;
+  const user = userDatabase[userId];
+  
+  if (!user) {
+    return res.status(401).send("Only logged in users can shorten URLs");
+  }
+  
+  const id = req.params.id;
+  if (!urlDatabase[id]) {
+    return res.status(404).send("That URL does not exist!");
+  }
+  const longURL = urlDatabase[id].longURL;
+
+  res.redirect(longURL);
+});
+
+app.get('/urls/:id', (req, res) => {
+  const userId = req.session.user_id;
+  const user = userDatabase[userId];
+  
+  if (!user) {
+    return res.status(401).send("Only logged in users can shorten URLs");
+  }
+  
+  const id = req.params.id;
+  if (!urlDatabase[id]) {
+    return res.status(404).send("That URL does not exist!");
+  }
+  const longURL = urlDatabase[id].longURL;
+  
+  const templateVars = { id, longURL, user };
+  res.render("urls_show", templateVars);
+});
 app.post("/urls/:id", (req, res) => {
   const id = req.session.user_id;
   const user = userDatabase[id];
   
   if (!user) {  //not logged in
-    return res.status(403).send("Only logged in users can shorten URLs");
+    return res.status(401).send("Only logged in users can shorten URLs");
   }
 
   //get url ID
@@ -169,57 +235,6 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[urlIDToUpdate].longURL = req.body.longURL;
   //don't need to use render as we don't need a new page, we just want to go back to our new updated homepage
   res.redirect("/urls"); //we don't pass any data back as the database gets redrawn on return to /urls
-});
-
-app.get("/login", (req, res) => {
-  const id = req.session.user_id;
-
-  if (id) {
-    res.redirect("/urls");
-    return;
-  }
-  const templateVars = {
-    user: null,
-  };
-  res.render("login", templateVars);
-});
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  const user = getUserByEmail(email, userDatabase);
-  if (!user) { //returned undefined or otherwise
-    return res.status(403).send('user not found');
-  }
-
-  if (!bcrypt.compareSync(password, user.password)) {
-    return res.status(403).send('the passwords do not match');
-  }
-  req.session.user_id = user.id;
-  res.redirect("/urls");
-});
-
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.redirect("/urls");
-});
-
-app.get('/urls/:id', (req, res) => {
-  const userId = req.session.user_id;
-  const user = userDatabase[userId];
-
-  if (!user) {
-    return res.status(403).send("Only logged in users can shorten URLs");
-  }
-
-  const id = req.params.id;
-  if (!urlDatabase[id]) {
-    return res.status(403).send("That's not a shortened URL ID!");
-  }
-  const longURL = urlDatabase[id].longURL;
-
-  const templateVars = { id, longURL, user };
-  res.render("urls_show", templateVars);
 });
 
 app.get('*', (req, res) => { //if we try to go to another page, we'll get sent to urls or login
